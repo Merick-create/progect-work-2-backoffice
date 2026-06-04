ca69371757f5b06498e830b24be2dac64ab42cc9
Merick-create <ric05bras@gmail.com>
2026-06-04 10:03:23 +0200

    sus

--- project-work-2-backoffice/src/app/app-routing.module.ts
+++ project-work-2-backoffice/src/app/app-routing.module.ts
@@ -34,7 +34,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: ''                 // opzionale: fallback alla home
+    redirectTo: ''                 
   },
   {
   path: 'profilo',

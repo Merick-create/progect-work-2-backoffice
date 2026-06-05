fbdae46b942d5b8d359375195dff17254468ab3f
Merick-create <ric05bras@gmail.com>
2026-06-05 14:18:16 +0200

    fix verification-success: Vai al login naviga a /login

--- project-work-2-backoffice/src/app/page/verification-success/verification-success.component.ts
+++ project-work-2-backoffice/src/app/page/verification-success/verification-success.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 export class VerificationSuccessComponent {
   constructor(private router: Router) {}
 
-  goToHome(): void {
-    this.router.navigate(['/home']);
+  goToLogin(): void {
+    this.router.navigate(['/login']);
   }
 }
